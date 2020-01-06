a74c2ca0f497b9b3d88ad0eb66385d373243e35d
Miguel Zetina <loefthur26@gmail.com>
2020-01-06 03:21:51 -0600

    Solve problem Python 1

--- primes.py
+++ primes.py
@@ -1,16 +1,29 @@
 """
-Your module documentation here
+Methods to manipulate prime numbers
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    Class to manipulate prime numbers
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Return True if the number is prime, otherwise, return False
+        using a simple primality test with 6k +/- 1 optimization
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        if num_int <= 1:
+            return False
+        if num_int <= 3:
+            return True
+        if (num_int % 2 == 0 or num_int % 3 == 0):
+            return False
+
+        auxiliar = 5
+        while auxiliar * auxiliar <= num_int:
+            if (num_int % auxiliar == 0 or num_int % (auxiliar + 2) == 0):
+                return False
+            auxiliar += 6
+
+        return True
