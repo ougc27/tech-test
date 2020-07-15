561e4839592e789267bb0479f30a0fe6f908cda6
Alexvachack <alexvachack>
2020-07-15 17:43:51 -0400

    Python Test 1 Xavier Vargas

--- primes.py
+++ primes.py
@@ -1,16 +1,25 @@
 """
-Your module documentation here
+Desarrollador: Vargas Contreras Xavier Alexis
+Version: 1.0
+Fecha: 15 Julio 2020
 """
+#La Clase PrimeClass contiene metodos para realizar operaciones con numeros primos
+class NumerosPrimos(object):
 
+    #El metodo is_prime verifica si un numero obtenido por un parametro llamado numero_entero
+    def verificar_numero_primo(self, numero_entero):
 
-class PrimeClass(object):
-    """
-    Your class documentation here
-    """
+        #Verificamos que el numero obtenido por parametro en el modulo sea mayor a 1
+        if numero_entero <= 1:
+            return False
 
-    def is_prime(self, num_int):
-        """
-        Your method documentation here
-        """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        #Verificamos que el numero sea solo divisible entre uno y el mismo
+        else:
+            for elemento in range(2, numero_entero):
+
+                #Retornamos False si el numero es divisible por otros numeros no es primo
+                if numero_entero % elemento == 0:
+                    return False
+
+            #Retornamos True si solo es divisible entre 1 y el mismo numero es un numero primo
+            return True
