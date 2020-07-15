58f7232d77d407e17af6b63dbba8954bf5332b4d
Alexvachack <alexvachack>
2020-07-15 18:02:28 -0400

    Python Test 2 Xavier

--- calculator.py
+++ calculator.py
@@ -1,16 +1,18 @@
 """
-Your module documentation here
+Desarrollador: Vargas Contreras Xavier Alexis
+Version: 1.0
+Fecha: 15 Julio 2020
 """
 
-
+#La clase CalculatorClass contiene metodos para realizar operaciones con numeros
 class CalculatorClass(object):
-    """
-    Your class documentation here
-    """
 
+    #El metodo sum realiza una suma de los numeros optenidos por el parameto num_list
     def sum(self, num_list):
-        """
-        Your method documentation here
-        """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+        sumador = 0
+        #Se realiza un recorrido de la lista sumando cada elemento
+        for elemento in range (0, len(num_list)):
+            sumador = sumador + num_list[elemento]
+
+        #Se Regresa el resultado de la suma de la lista
+        return sumador
