f31d8633ec1fdaef809926937a1e86ca303d6ab5
Alexvachack <alexvachack>
2020-07-15 20:35:01 -0400

    SQL 2 Test Xavier Vargas

--- employee.sql
+++ employee.sql
@@ -1,55 +1,52 @@
 -- Desarrollador: Vargas Contreras Xavier Alexis
 -- Version 1:0
---SQL TEST 1
+--SQL TEST 2
 
--- 1. Creacion de base de datos llamada "employee_employee"
-CREATE DATABASE employee_employee
-    WITH 
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    CONNECTION LIMIT = -1;
--- 2. Crea una tabla llamada "employee" con los campos "id", "first_name", "last_name". 
-CREATE TABLE public.employee
+-- 1. Crea una tabla llamada "employee_hobby" con los campos "id", "name", "description".
+CREATE TABLE public.employee_hobby
 (
-    id integer NOT NULL,
-    first_name character varying NOT NULL,
-    last_name character varying NOT NULL,
-    PRIMARY KEY (id)
-);
---3. Crea una tabla "employee_department" con los campos "id", "name", "description".
-CREATE TABLE public.employee_department
-(
-    id integer NOT NULL,
+    id integer,
     name character varying,
     description character varying,
     PRIMARY KEY (id)
 );
--- 4. Haz los cambios necesarios en la base de datos para poder asignarle a los empleados su respectivo departamento.
-ALTER TABLE public.employee
-    ADD COLUMN "CodigoDepartamento" integer;
+-- 2. Haz los cambios necesarios en la base de datos para poder asignar a los empleados sus pasatiempos.
+CREATE TABLE public.hobbie_employee
+(
+    id_employee integer,
+    id_hobbie integer,
+    id integer,
+    PRIMARY KEY (id)
+);
 
-ALTER TABLE public.employee
-    ADD CONSTRAINT "CodigoDepartamento" FOREIGN KEY ("CodigoDepartamento")
-    REFERENCES public.employee_department (id);
--- Inserta 4 empleados y 6 departamentos de datos, asígnale a los empleados un departamento. 
-INSERT INTO public.employee_department
-	VALUES ('1','Departamento 1','Color Azul');
-INSERT INTO public.employee_department
-        VALUES ('2','Departamento 2','Color Rojo');
-INSERT INTO public.employee_department
-        VALUES ('3','Departamento 3','Color Negro');
-INSERT INTO public.employee_department
-        VALUES ('4','Departamento 4','Color Blanco');
-INSERT INTO public.employee_department
-        VALUES ('5','Departamento 5','Color Rosa');
-INSERT INTO public.employee_department
-        VALUES ('6','Departamento 6','Color Morado');
+ALTER TABLE public.hobbie_employee
+    ADD FOREIGN KEY (id_hobbie)
+    REFERENCES public.employee_hobby (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+-- 3. Inserta 3 pasatiempos y asígnale a todos los empleados al menos 2 pasatiempos. 
 
-INSERT INTO public.employee
-	VALUES ('1','Xavier','Vargas','1');
-INSERT INTO public.employee
-	VALUES ('2','Ricardo','Contreras','4');
-INSERT INTO public.employee
-	VALUES ('3','Fernando','Sanchez','3');
-INSERT INTO public.employee
-	VALUES ('4','Eduardo','Gonzales','2');
+INSERT INTO public.employee_hobby
+	VALUES ('1','Correr','Deporte');
+INSERT INTO public.employee_hobby     
+        VALUES ('2','Bailar','Arte');
+INSERT INTO public.employee_hobby     
+        VALUES ('3','Estudiar','Aprendizaje');
+
+INSERT INTO public.hobbie_employee    
+        VALUES ('1','1','1');
+INSERT INTO public.hobbie_employee    
+        VALUES ('1','1','2');
+INSERT INTO public.hobbie_employee    
+        VALUES ('1','2','3')
+INSERT INTO public.hobbie_employee    
+        VALUES ('1','2','1');
+INSERT INTO public.hobbie_employee    
+        VALUES ('1','3','1');
+INSERT INTO public.hobbie_employee    
+        VALUES ('1','4','2');
+
+
+;
+
