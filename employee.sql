5b90d43d395e60067ba5217dd885488b0e94f9c5
JavierRG <37031230+JavierRG@users.noreply.github.com>
2020-07-27 20:26:21 -0500

    Employee test2

--- employee.sql
+++ employee.sql
@@ -1,37 +1,25 @@
--- Your sql code in this file
--- NOTE: Please, don't add sentence to create database in this script file.
---       You can create database locally to test it.
---       Consider add ';' at end sentence.
---
--- CREATE DATABASE employee_employee;
-
-CREATE TABLE employee (
-    id int IDENTITY(1,1) PRIMARY KEY,
-    first_name varchar(255),
-    last_name varchar(255) 
-);
-
-CREATE TABLE employee_department (
+CREATE TABLE employee_hobby (
     id int IDENTITY(1,1) PRIMARY KEY,
     name varchar(255),
     description varchar(255)
 );
 
-ALTER TABLE employee
-ADD id_department int,
-FOREIGN KEY(id_department) REFERENCES  employee_department(id);
+CREATE TABLE hobbys (
+    id int NOT NULL PRIMARY KEY,
+    Employee_ID int,
+    FOREIGN KEY (Employee_ID) REFERENCES employee(id),
+    Hobby_ID int,
+    FOREIGN KEY (Hobby_ID) REFERENCES employee_hobby(id)
+);
 
-INSERT INTO employee_department 
-VALUES
-('Desarollo', 'Programadores'),
-('RH', 'Recursos Humanos'),
-('Marketing', 'Diseñadores'),
-('Ingenieria', 'Sistemas y soporte'),
-('Ventas', 'Servicios'),
-('Administracion', 'Facturacion');
+INSERT INTO employee_hobby VALUES
+(1, 'Programacion', 'Sistemas'),
+(2, 'Futbol', 'Deporte'),
+(3, 'Ejercicio', 'Gimnasio');
 
-INSERT INTO employee VALUES
-('Javier', 'Ramirez','1'),
-('Oliver', 'Cervantez','2'),
-('Bob', 'Gonzalez','1'),
-('Jesus', 'Guzman','5');
+INSERT INTO hobbys VALUES
+('1','1'),
+('1','2'),
+('1','3'),
+('2','1'),
+('2','3');
