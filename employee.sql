c16863fe02e820ac20b725e6305390a9e19959a8
JavierRG <37031230+JavierRG@users.noreply.github.com>
2020-07-27 21:11:19 -0500

    Employee test 1

--- employee.sql
+++ employee.sql
@@ -4,12 +4,32 @@
 --       Consider add ';' at end sentence.
 
 CREATE TABLE employee (
+    id int IDENTITY(1,1) PRIMARY KEY,
+    first_name varchar(255),
+    last_name varchar(255) 
 );
 
 CREATE TABLE employee_department (
+    id int IDENTITY(1,1) PRIMARY KEY,
+    name varchar(255),
+    description varchar(255)
 );
 
-CREATE TABLE employee_hobby (
-);
+ALTER TABLE employee
+ADD id_department int,
+FOREIGN KEY(id_department) REFERENCES  employee_department(id);
 
--- ...
+INSERT INTO employee_department 
+VALUES
+('Desarollo', 'Programadores'),
+('RH', 'Recursos Humanos'),
+('Marketing', 'Diseñadores'),
+('Ingenieria', 'Sistemas y soporte'),
+('Ventas', 'Servicios'),
+('Administracion', 'Facturacion');
+
+INSERT INTO employee VALUES
+('Javier', 'Ramirez','1'),
+('Oliver', 'Cervantez','2'),
+('Bob', 'Gonzalez','1'),
+('Jesus', 'Guzman','5');
