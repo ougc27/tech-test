e5e6c2ad56bbce62dd57077dd66b3b3edeb3e6d8
JavierRG <37031230+JavierRG@users.noreply.github.com>
2020-07-27 20:26:54 -0500

    Employee test 3

--- employee.sql
+++ employee.sql
@@ -1,25 +1,6 @@
-CREATE TABLE employee_hobby (
-    id int IDENTITY(1,1) PRIMARY KEY,
-    name varchar(255),
-    description varchar(255)
-);
+ALTER TABLE employee
+ADD BossID int,
+FOREIGN KEY(BossID) REFERENCES  employee(id);
 
-CREATE TABLE hobbys (
-    id int NOT NULL PRIMARY KEY,
-    Employee_ID int,
-    FOREIGN KEY (Employee_ID) REFERENCES employee(id),
-    Hobby_ID int,
-    FOREIGN KEY (Hobby_ID) REFERENCES employee_hobby(id)
-);
-
-INSERT INTO employee_hobby VALUES
-(1, 'Programacion', 'Sistemas'),
-(2, 'Futbol', 'Deporte'),
-(3, 'Ejercicio', 'Gimnasio');
-
-INSERT INTO hobbys VALUES
-('1','1'),
-('1','2'),
-('1','3'),
-('2','1'),
-('2','3');
+UPDATE employee
+SET BossID = '3'
