9cb4670e32453fbe08b9ab8e5dbcfcadc46b3cd8
JavierRG <37031230+JavierRG@users.noreply.github.com>
2020-07-27 20:52:04 -0500

    Primes Python Test 2

--- primes.py
+++ primes.py
@@ -1,16 +1,29 @@
 """
-Your module documentation here
+Modulo de insercion de valores en donde se valida si son numeros primarios o no.
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+ 
+    Clase de inicializacion de funciones para validacion de datos.
     """
+
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        La funcion is_prime requiere un valor para determinar si es un numero
+        primario o si no lo es. Condicionando que un número primo es un
+        número que solo es divisible por sí solo y por 1.
+        
         """
         # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        if num_int <  2:
+            return False
+        for i in range(2, num_int):
+            if num_int % i == 0:
+                return False
+        return True
+valor = int(input("Introduzca un numero: "))
+primaro = PrimeClass()
+primaro.is_prime(valor)
