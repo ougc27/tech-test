9e0236cd0487a5a2e341049e031dccea054efec4
JavierRG <37031230+JavierRG@users.noreply.github.com>
2020-07-27 20:25:36 -0500

    CalculatorPython test 2

--- calculator.py
+++ calculator.py
@@ -1,16 +1,33 @@
 """
-Your module documentation here
+Script de suma de variables dentro de una lista.
 """
 
 
 class CalculatorClass(object):
     """
-    Your class documentation here
+    Esta clase permite la adicion de datos dentro de una lista para obtener una
+     suma de los mismos.
     """
 
     def sum(self, num_list):
         """
-        Your method documentation here
+        Al correr el script estara pidiendo datos y agregandolos a una lista
+         hasta que se le ingrese el valor '0' para terminar la ejecucion del
+          while y asi avanzar a realizar la suma de los valores dentro de la
+           lista e imprimirlos.
         """
         # your sum code here
-        return "not implement yet"  # Remove this dummy line
+        Datos = []
+        while num_list != "0":
+            Datos.append(num_list)
+            num_list = int(input(
+                "Introduzca un numero (Introduzca '0' para finalizar):"))
+            
+        else:
+            Suma = sum(Datos)
+            print(Suma)
+
+
+numeros = int(input("Introduzca un numero (Introduzca '0' para finalizar): "))
+primaro = CalculatorClass()
+primaro.sum(numeros)
