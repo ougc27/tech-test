64f100075a7d07379712ca84f02a3ea3f941679a
IrisGabrielaArrona <irisgabriela.arrona@gmail.com>
2021-06-09 18:49:16 -0500

    SQL test 2

--- employee.sql
+++ employee.sql
@@ -23,7 +23,60 @@ CREATE TABLE employee_department (
 		REFERENCES employee_department(id)
 );
 
+--VALUES
+
+/*INSERT INTO employee_department
+VALUES ('100','Desarrollo','Equipo de desarrollo de software'),
+('101','Ventas','Equipo de ventas'),
+('102','Recursos Humanos','Equipo de reclutamiento'),
+('103','Pruebas','Equipo de Testing'),
+('104','Marketing','Equipo de Mercadotecnia'),
+('105','Produccion','Equipo de Produccion');
+
+INSERT INTO employee
+VALUES ('1','Iris Gabriela','Arrona','100'),
+('2','Elizabeth','Villareal','102'),
+('3','Oscar','Garza','100'),
+('4','Luis Adolfo','Guevara','101');
+*/
+
 CREATE TABLE employee_hobby (
+	id INT PRIMARY KEY,
+	name VARCHAR(40) NOT NULL,
+	description VARCHAR(60) NOT NULL
 );
+
+CREATE TABLE employee_join_hobby (
+	employee_id INT REFERENCES employee(id),
+	hobby_id INT REFERENCES employee_hobby(id),
+	constraint id PRIMARY KEY (employee_id, hobby_id)
+);
+
+
+--VALUES
+
+/*
+INSERT INTO employee_hobby
+VALUES ('1','Bailar','arte escenica\actividad fisica'),
+('2','Pintar','Artes plasticas'),
+('3','Videojuegos','Realidad Virtual');
+
+
+INSERT INTO employee_join_hobby
+VALUES ('1','1'),
+('1','3'),
+('2','1'),
+('2','2'),
+('3','2'),
+('3','3'),
+('4','1'),
+('4','2')
+;
+*/
+
+--SQL STATEMENT TO SHOW THE MANY 2 MANY RELATION BETWEEN EMPLOYEES AND HOBBIES
+/*SELECT * FROM employee_join_hooby MM join employee E on (MM.student_id = E.id)*/
+
+
 
 -- ...
