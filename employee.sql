afc79371bc79229f92ee25ed19853ee428fe6b47
IrisGabrielaArrona <irisgabriela.arrona@gmail.com>
2021-06-09 18:02:58 -0500

    SQL test 1

--- employee.sql
+++ employee.sql
@@ -3,10 +3,24 @@
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
 
+--create database command -> sudo -u postgree createdb employee_employee
+
 CREATE TABLE employee (
+	id INT PRIMARY KEY,
+	first_name VARCHAR(40) NOT NULL,
+	last_name VARCHAR(40) NOT NULL,
+	id_department INT NOT NULL,
+	FOREIGN KEY (id_department)
+		REFERENCES employee_department(id)
 );
 
 CREATE TABLE employee_department (
+	id INT PRIMARY KEY,
+	first_name VARCHAR(40) NOT NULL,
+	last_name VARCHAR(40) NOT NULL,
+	id_department INT NOT NULL,
+	FOREIGN KEY (id_department)
+		REFERENCES employee_department(id)
 );
 
 CREATE TABLE employee_hobby (
