679013d27179f80e3580fc83e240f747494faca9
IrisGabrielaArrona <irisgabriela.arrona@gmail.com>
2021-06-09 19:41:57 -0500

    SQL test 3

--- employee.sql
+++ employee.sql
@@ -25,7 +25,7 @@ CREATE TABLE employee_department (
 
 --VALUES
 
-/*INSERT INTO employee_department
+INSERT INTO employee_department
 VALUES ('100','Desarrollo','Equipo de desarrollo de software'),
 ('101','Ventas','Equipo de ventas'),
 ('102','Recursos Humanos','Equipo de reclutamiento'),
@@ -38,7 +38,7 @@ VALUES ('1','Iris Gabriela','Arrona','100'),
 ('2','Elizabeth','Villareal','102'),
 ('3','Oscar','Garza','100'),
 ('4','Luis Adolfo','Guevara','101');
-*/
+
 
 CREATE TABLE employee_hobby (
 	id INT PRIMARY KEY,
@@ -55,7 +55,7 @@ CREATE TABLE employee_join_hobby (
 
 --VALUES
 
-/*
+
 INSERT INTO employee_hobby
 VALUES ('1','Bailar','arte escenica\actividad fisica'),
 ('2','Pintar','Artes plasticas'),
@@ -72,11 +72,35 @@ VALUES ('1','1'),
 ('4','1'),
 ('4','2')
 ;
-*/
+
 
 --SQL STATEMENT TO SHOW THE MANY 2 MANY RELATION BETWEEN EMPLOYEES AND HOBBIES
 /*SELECT * FROM employee_join_hooby MM join employee E on (MM.student_id = E.id)*/
 
 
 
--- ...
+CREATE TABLE boss_join_employee (
+	boss_id INT REFERENCES employee(id),
+	employee_id INT UNIQUE REFERENCES employee(id),
+	constraint b_id PRIMARY KEY (boss_id, employee_id)
+);
+
+
+INSERT INTO boss_join_employee
+VALUES ('2','1'),
+('3','2'),
+('3','4'),
+('2','3')
+;
+
+ALTER TABLE employee
+ADD COLUMN boss_id INT REFERENCES employee(id)
+
+UPDATE employee
+SET boss_id = BE.boss_id
+FROM boss_join_employee AS BE
+WHERE employee.id = BE.employee_id
+
+
+
+
