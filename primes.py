34c340d90cc44d5c69e609979220f64b62616694
Francisco Castillo <francisco.castillo@meridasoft.com>
2021-06-17 14:23:33 -0500

    python Test1

--- primes.py
+++ primes.py
@@ -4,13 +4,22 @@ Your module documentation here
 
 
 class PrimeClass(object):
-    """
-    Your class documentation here
-    """
-
-    def is_prime(self, num_int):
-        """
-        Your method documentation here
-        """
+    """Clase en la cual encontraremos la funcion
+        para verificar si algun numero es primo"""
+    def is_prime(self, num):
+        """Funcion para verificar si un numero es primo
+        para determinarlo tiene que ser divisible dos veces por 1 y por
+        si mismo Iniciamos con un for desde la posicion 2 hasta llegar
+        a nuestro numero si se encuentra una division antes de llegar a
+        dicho numero quiere decir que no es primo"""
         # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        for i in range(2, num):
+            if num % i == 0:
+                print('No es Primo')
+                return False
+        print('Es Primo')
+        return True
+
+#instancia = PrimeClass()
+
+#resultado = instancia.is_prime(5)
